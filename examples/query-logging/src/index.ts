7b8e8134ca41afac88c071fcc1052d43961a3979
Stephen Brian King <3913213+sbking@users.noreply.github.com>
2022-12-07 08:04:36 -0700

    Add query-logging example

---
+++ examples/query-logging/src/index.ts
@@ -0,0 +1,43 @@
+import { performance } from "perf_hooks";
+import { PrismaClient } from "./generated/client";
+import * as util from "util";
+
+const prisma = new PrismaClient().$extends({
+  name: "query-logging",
+  query: {
+    $allModels: {
+      async $allOperations({ operation, model, args, query }) {
+        const start = performance.now();
+        const result = await query(args);
+        const end = performance.now();
+        const time = end - start;
+        console.log(
+          util.inspect(
+            { model, operation, args, time },
+            { showHidden: false, depth: null, colors: true }
+          )
+        );
+        return result;
+      },
+    },
+  },
+});
+
+async function main() {
+  await prisma.user.findMany({
+    orderBy: [{ lastName: "asc" }, { firstName: "asc" }],
+    take: 5,
+  });
+
+  await prisma.user.groupBy({ by: ["lastName"], _count: true });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
