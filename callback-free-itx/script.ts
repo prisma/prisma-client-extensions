cfd9949d8279eca3d627cf4aab157e59b3b9e052
Alex Ruheni <33921841+ruheni@users.noreply.github.com>
2023-08-15 15:54:07 +0000

    chore: fix typing in callback-free-itx example

--- callback-free-itx/script.ts
+++ callback-free-itx/script.ts
@@ -7,63 +7,68 @@ type FlatTransactionClient = Prisma.TransactionClient & {
 
 const ROLLBACK = { [Symbol.for("prisma.client.extension.rollback")]: true };
 
-const prisma = new PrismaClient({ log: ["query"] }).$extends({
-  client: {
-    async $begin() {
-      const prisma = Prisma.getExtensionContext(this);
-      let setTxClient: (txClient: Prisma.TransactionClient) => void;
-      let commit: () => void;
-      let rollback: () => void;
+const callbackFreeItx = Prisma.defineExtension((client) => {
+  return client.$extends({
+    client: {
+      async $begin() {
+        const prisma = Prisma.getExtensionContext(this);
+        let setTxClient: (txClient: Prisma.TransactionClient) => void;
+        let commit: () => void;
+        let rollback: () => void;
 
-      // a promise for getting the tx inner client
-      const txClient = new Promise<Prisma.TransactionClient>((res) => {
-        setTxClient = (txClient) => res(txClient);
-      });
-
-      // a promise for controlling the transaction
-      const txPromise = new Promise((_res, _rej) => {
-        commit = () => _res(undefined);
-        rollback = () => _rej(ROLLBACK);
-      });
-
-      // opening a transaction to control externally
-      if (
-        "$transaction" in prisma &&
-        typeof prisma.$transaction === "function"
-      ) {
-        const tx = prisma.$transaction((txClient) => {
-          setTxClient(txClient);
-
-          return txPromise.catch((e) => {
-            if (e === ROLLBACK) return;
-            throw e;
-          });
+        // a promise for getting the tx inner client
+        const txClient = new Promise<Prisma.TransactionClient>((res) => {
+          setTxClient = (txClient) => res(txClient);
         });
 
-        // return a proxy TransactionClient with `$commit` and `$rollback` methods
-        return new Proxy(await txClient, {
-          get(target, prop) {
-            if (prop === "$commit") {
-              return () => {
-                commit();
-                return tx;
-              };
-            }
-            if (prop === "$rollback") {
-              return () => {
-                rollback();
-                return tx;
-              };
-            }
-            return target[prop as keyof typeof target];
-          },
-        }) as FlatTransactionClient;
-      }
+        // a promise for controlling the transaction
+        const txPromise = new Promise((_res, _rej) => {
+          commit = () => _res(undefined);
+          rollback = () => _rej(ROLLBACK);
+        });
 
-      throw new Error("Transactions are not supported by this client");
-    },
-  },
-});
+        // opening a transaction to control externally
+        if (
+          "$transaction" in prisma &&
+          typeof prisma.$transaction === "function"
+        ) {
+          const tx = prisma.$transaction((txClient) => {
+            setTxClient(txClient as unknown as Prisma.TransactionClient);
+
+            return txPromise.catch((e) => {
+              if (e === ROLLBACK) return;
+              throw e;
+            });
+          });
+
+          // return a proxy TransactionClient with `$commit` and `$rollback` methods
+          return new Proxy(await txClient, {
+            get(target, prop) {
+              if (prop === "$commit") {
+                return () => {
+                  commit();
+                  return tx;
+                };
+              }
+              if (prop === "$rollback") {
+                return () => {
+                  rollback();
+                  return tx;
+                };
+              }
+              return target[prop as keyof typeof target];
+            },
+          }) as FlatTransactionClient;
+        }
+
+        throw new Error("Transactions are not supported by this client");
+      },
+    }
+  })
+})
+
+const prisma = new PrismaClient({ log: ['query'] }).$extends(callbackFreeItx)
+
 
 async function main() {
   const tx = await prisma.$begin();
